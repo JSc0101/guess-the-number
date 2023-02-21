c17fff2a02404e34e8f8e8f756cfd1f9a86aba7a
Sebastian009w <ia3266505@gmail.com>
2023-02-21 09:28:58 -0500

    condiciones al numero a divinar

--- main.py
+++ main.py
@@ -6,8 +6,24 @@ max_number = 20
 
 print("!Hello¡ What your name ?: ")
 
+# nombre de usuario
 username = input()
 
+number__ale = random.randint(min__number, max_number)
 print(
     f"Welcome {username} I am thinking in a number between {str(min__number)} and  {str(max_number)}"
 )
+
+# Bucle del juego
+while guess__taken < 6:
+    print("take a guess: ")
+    guess = input()
+    guess = int(guess)
+
+    guess__taken  += 1  
+    if guess < number__ale:
+        print("Your guess is too low")
+    if guess > number__ale:
+        print("Your guess is to high")
+    if guess == number__ale:
+        break
