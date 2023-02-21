72204c23b59e65e063b8271f920388fa7c3e3fae
Sebastian009w <ia3266505@gmail.com>
2023-02-21 09:21:19 -0500

    se captura el nombre de usuario

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+import random
+
+guess__taken = 0
+min__number = 1
+max_number = 20
+
+print("!Hello¡ What your name ?: ")
+
+username = input()
+
+print(
+    f"Welcome {username} I am thinking in a number between {str(min__number)} and  {str(max_number)}"
+)
